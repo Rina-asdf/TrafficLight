60c13d5b258e12ee35bbe8b1934a3dda19c9588c
Rina-asdf <rina1000work@gmail.com>
2024-01-23 22:24:04 +0900

    commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const button =document.getElementById("hello_btn");
+/* const button =document.getElementById("left");
 
 function hello() {
 
@@ -6,4 +6,28 @@ function hello() {
     
     }
 
-button.onclick = hello;
+leftdiv.addEventListener('click' , hello) */
+
+/* document.addEventListener('DOMContentLoaded', function() {
+    const redLamp = document.getElementById('red');
+    const yellowLamp = document.getElementById('yellow');
+    const greenLamp = document.getElementById('green');
+
+    redLamp.addEventListener('click', function() {
+        redLamp.style.backgroundColor = 'red';
+        yellowLamp.style.backgroundColor = 'gray';
+        greenLamp.style.backgroundColor = 'gray';
+    });
+
+    yellowLamp.addEventListener('click', function() {
+        redLamp.style.backgroundColor = 'gray';
+        yellowLamp.style.backgroundColor = 'yellow';
+        greenLamp.style.backgroundColor = 'gray';
+    });
+
+    greenLamp.addEventListener('click', function() {
+        redLamp.style.backgroundColor = 'gray';
+        yellowLamp.style.backgroundColor = 'gray';
+        greenLamp.style.backgroundColor = 'green';
+    });
+}); */
